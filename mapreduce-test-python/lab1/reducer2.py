2693e96cba44e58b45d45f84b29b9be430f47753
akayou_adane <akuadane1205@gmail.com>
2025-02-15 12:29:54 -0500

    edited print format

--- mapreduce-test-python/lab1/reducer2.py
+++ mapreduce-test-python/lab1/reducer2.py
@@ -3,7 +3,8 @@ import sys
 
 # This function print the three lines stored in the array prev
 def print_ips(prev):
-    for line in prev:
+
+    for line in prev[::-1]:
         # skip if the line is empty, in case of hours that have less than three different ip visits
         if line!="":
             print(line)
@@ -15,19 +16,24 @@ prev=["","",""]
 for line in sys.stdin:
     line = line.strip()
     hour,count_ip  = tuple(line.split('-'))
+    count,ip = tuple(count_ip.split('\t'))
+    try: 
+        count = int(count)
+        # shift the line by one since the incoming will definetly have more count
+        if cur_hour==hour:
+            prev[0]= prev[1]
+            prev[1]= prev[2]
+            prev[2]= "%s\t%s"%(hour+ip,count)
+        else:
+            # if the line has new hour, print the previous three lines
+            if cur_hour:
+                print_ips(prev)
+            # reset cur_hour and prev
+            cur_hour = hour
+            prev=["","",""]
+            prev[2] = "%s\t%s"%(hour+ip,count)
+    except:
+        pass
 
-    # shift the line by one since the incoming will definetly have more count
-    if cur_hour==hour:
-        prev[0]= prev[1]
-        prev[1]= prev[2]
-        prev[2]= line
-    else:
-        # if the line has new hour, print the previous three lines
-        if cur_hour:
-            print_ips(prev)
-        # reset cur_hour and prev
-        cur_hour = hour
-        prev=["","",""]
-        prev[2] = line
 
 print_ips(prev)
