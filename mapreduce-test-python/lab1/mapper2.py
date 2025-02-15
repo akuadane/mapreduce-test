602586ab70436b0ce130afe3e79c534eac38d1d1
akayou_adane <akuadane1205@gmail.com>
2025-02-15 12:26:10 -0500

    converted count to int

--- mapreduce-test-python/lab1/mapper2.py
+++ mapreduce-test-python/lab1/mapper2.py
@@ -4,7 +4,11 @@ import sys
 for line in sys.stdin:
     line = line.strip()
     hour_ip, count = tuple(line.split('\t'))
-    hour,ip = tuple(hour_ip.split(']'))
-    hour+=']'
+    try:
+        count = int(count)
+        hour,ip = tuple(hour_ip.split(']'))
+        hour+=']'
 
-    print '%s\t%s'%(hour+"-"+"{:05d}".format(count),ip)
+        print '%s\t%s'%(hour+"-"+"{:05d}".format(count),ip)
+    except: 
+        pass
