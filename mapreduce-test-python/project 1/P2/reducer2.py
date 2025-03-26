45989de054d43b3accf8feee7c071a3138688446
akayou_adane <akuadane1205@gmail.com>
2025-03-26 17:22:53 -0400

    implemented reducer

---
+++ mapreduce-test-python/project 1/P2/reducer2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+
+prev_player = None
+defender = None
+for line in sys.stdin:
+    line = line.strip()
+    
+    key, defender= line.split("\t")
+
+    player,_ = key.split('-')
+
+    # print the first occurence of the key
+    if prev_player!=player:
+        prev_player = player
+        print("%s\t%s"%(player,defender))
+    
+    
+    
