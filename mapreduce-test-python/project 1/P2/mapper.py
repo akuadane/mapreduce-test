d8cc4da70b2e1341f3d278b2e8990db1968631b3
akayou_adane <akuadane1205@gmail.com>
2025-03-26 11:38:14 -0400

    index changed

--- mapreduce-test-python/project 1/P2/mapper.py
+++ mapreduce-test-python/project 1/P2/mapper.py
@@ -6,6 +6,7 @@ for line in sys.stdin:
     fields = line.split(",")
     
     # make the player name and defender the key 
-    key = "%s-%s" % (fields[19],fields[14])
-    value  = 1 if fields[13]=='made' else 0
+    
+    key = "%s-%s" % (fields[20],fields[15])
+    value  = 1 if fields[14]=='made' else 0
     print "%s\t%s" % (key, value)
