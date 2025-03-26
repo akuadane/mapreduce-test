7861c7886115e43b978c4a56e8d9c45a0cdde008
akayou_adane <akuadane1205@gmail.com>
2025-03-26 11:58:43 -0400

    changed file names and implemented reducer1

--- mapreduce-test-python/project 1/P2/mapper2.py
+++ mapreduce-test-python/project 1/P2/mapper2.py
@@ -6,7 +6,8 @@ for line in sys.stdin:
     fields = line.split(",")
     
     # make the player name and defender the key 
-    
     key = "%s-%s" % (fields[22],fields[17])
+
+    # For the value, use 1 if the player made the shot, 0 otherwise.
     value  = 1 if fields[14]=='made' else 0
     print "%s\t%s" % (key, value)
---
+++ mapreduce-test-python/project 1/P2/reducer1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import sys
+
+prev_key = None
+made=total=0
+for line in sys.stdin:
+    line = line.strip()
+    
+    key, value= line.split("\t")
+
+    # convert the shot value to int
+    value = int(value)
+    if prev_key == key:
+        made+=value
+        total+=1
+    else:
+        if prev_key!=None:
+            hit_score = str(round(made/total,2))
+            print "%s\t%s"%(prev_key,hit_score)
+        
+        prev_key = key
+        made  = value
+        total = 1
+        
+hit_score = str(round(made/total,2))
+print "%s\t%s"%(prev_key,hit_score)
+    
+    
