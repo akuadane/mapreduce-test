a5d72d0bbd1c756fcd4c10cbe07bb9ece1f37c79
akayou_adane <akuadane1205@gmail.com>
2025-03-26 12:50:48 -0400

    spliting changed

--- mapreduce-test-python/project 1/P2/mapper1.py
+++ mapreduce-test-python/project 1/P2/mapper1.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 import sys
+import re
+
 for line in sys.stdin:
     line = line.strip()
     
-    fields = line.split(",")
+    fields = re.split(r',(?=(?:[^"]*"[^"]*")*[^"]*$)', line)
     
     # make the player name and defender the key 
-    key = "%s-%s" % (fields[22],fields[17])
+    key = "%s-%s" % (fields[20],fields[15])
 
     # For the value, use 1 if the player made the shot, 0 otherwise.
-    value  = 1 if fields[14]=='made' else 0
+    value  = 1 if fields[13]=='made' else 0
     print "%s\t%s" % (key, value)
