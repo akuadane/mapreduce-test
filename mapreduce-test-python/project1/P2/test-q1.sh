fa7fff2129f9c77ccb33fbf8ac9900ee86a9c83b
akayou_adane <akuadane1205@gmail.com>
2025-03-30 16:39:53 -0400

    changed folder name again

--- mapreduce-test-python/project1/P2/test-q1.sh
+++ mapreduce-test-python/project1/P2/test-q1.sh
@@ -8,11 +8,11 @@
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../../mapreduce-test-data/shot_logs.csv /P2/input/
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ./mapper1.py -mapper ./mapper1.py -file ./reducer1.py -reducer ./reducer1.py \
+-file ./q1/mapper1.py -mapper ./q1/mapper1.py -file ./q1/reducer1.py -reducer ./q1/reducer1.py \
 -input /P2/input/shot_logs.csv -output /P2/output/ 
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ./mapper2.py -mapper ./mapper2.py -file ./reducer2.py -reducer ./reducer2.py \
+-file ./q1/mapper2.py -mapper ./q1/mapper2.py -file ./q1/reducer2.py -reducer ./q1/reducer2.py \
 -input /P2/output/* -output /P2/output2/
 
 /usr/local/hadoop/bin/hdfs dfs -cat /P2/output2/part-00000
