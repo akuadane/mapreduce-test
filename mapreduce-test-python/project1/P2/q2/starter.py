f2944293ea66d06ff28b7eb60c216dee4662ce6a
akayou_adane <akuadane1205@gmail.com>
2025-03-30 17:46:02 -0400

    corrected path

--- mapreduce-test-python/project1/P2/q2/starter.py
+++ mapreduce-test-python/project1/P2/q2/starter.py
@@ -5,10 +5,10 @@ command = [
     "/usr/local/hadoop/bin/hadoop", "jar", "/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar",
     "-input", "/P2/input/shot_logs.csv",
     "-output", "/P2/output/",
-    "-mapper", "../q1/mapper1.py",
-    "-reducer", "../q1/reducer1.py",
-    "-file", "../q1/mapper1.py",
-    "-file", "../q1/reducer1.py",
+    "-mapper", "./q1/mapper1.py",
+    "-reducer", "./q1/reducer1.py",
+    "-file", "./q1/mapper1.py",
+    "-file", "./q1/reducer1.py",
     # "-file", centroids_file  # Pass the current centroids file to the mappers
 ]
 
@@ -18,10 +18,10 @@ command = [
     "/usr/local/hadoop/bin/hadoop", "jar", "/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar",
     "-input", "/P2/output/*",
     "-output", "/P2/output2/",
-    "-mapper", "../q1/mapper2.py",
-    "-reducer", "../q1/reducer2.py",
-    "-file", "../q1/mapper2.py",
-    "-file", "../q1/reducer2.py",
+    "-mapper", "./q1/mapper2.py",
+    "-reducer", "./q1/reducer2.py",
+    "-file", "./q1/mapper2.py",
+    "-file", "./q1/reducer2.py",
     # "-file", centroids_file  # Pass the current centroids file to the mappers
 ]
 
